2e134dbcc95952dee69b6c51835f07b38926a980
Carl Phillips <Carl_Phillips@epam.com>
2020-06-16 16:50:22 +0200

    Added immer for reducer.

--- src/reducer.js
+++ src/reducer.js
@@ -1,23 +1,56 @@
+import { produce } from "immer";
 import * as actions from "./actionTypes";
 
 let lastId = 0;
 
 export default function reducer(state = [], action) {
-    switch(action.type) {
-        case actions.BUG_ADDED:
-            return [
-                ...state,
-                {
+    return produce(state, draftState => {
+        switch(action.type) {
+            case actions.BUG_ADDED:
+            {
+                draftState.push({
                     id: ++lastId,
                     description: action.payload.description,
                     resolved: false
-                }
-            ];
-        case actions.BUG_REMOVE:
-            return state.filter(bug => bug.id !== action.payload.id);
-        case actions.BUG_RESOLVED:
-            return state.map(bug => bug.id !== action.payload.id ? bug : { ...bug, resolved: true});
-        default:
-            return state;
-    }
+                });
+            }
+            case actions.BUG_REMOVE:
+            {
+                draftState.filter(bug => bug.id !== action.payload.id);
+            }
+            case actions.BUG_RESOLVED:
+            {
+                draftState.map(bug => {
+                    if(bug.id === action.payload.id) {
+                        bug.resolved = true;
+                        return bug;
+                    }
+                });
+            }
+            default:
+            {
+                draftState = [];
+            }
+        }
+    });
 }
+
+// export default function reducer(state = [], action) {
+//     switch(action.type) {
+//         case actions.BUG_ADDED:
+//             return [
+//                 ...state,
+//                 {
+//                     id: ++lastId,
+//                     description: action.payload.description,
+//                     resolved: false
+//                 }
+//             ];
+//         case actions.BUG_REMOVE:
+//             return state.filter(bug => bug.id !== action.payload.id);
+//         case actions.BUG_RESOLVED:
+//             return state.map(bug => bug.id !== action.payload.id ? bug : { ...bug, resolved: true});
+//         default:
+//             return state;
+//     }
+// }
