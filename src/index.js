1da87aeafc54af281a6a9e8c7e894d5816748dbd
Carl Phillips <Carl_Phillips@epam.com>
2020-06-16 14:55:49 +0200

    immutable example - immer produce example.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,14 @@
-const person = { name: "John",
-    address: {
-        country: "USA",
-        city: "San Francisco"
-    }
-};
-const clonedPerson = Object.assign({}, person);
-const updated = {
-    ...person,
-    address: {
-        ...person.address,
-        city: "`New York`"
-    },
-    name: "Carl",
-    age: 30
+import produce, { Produce } from "immer"
+
+let book = { title: "Harry Potter" };
+
+function publish(book) {
+    return produce(book, draftBook => {
+        draftBook.isPublished = true;
+    });
+    // book.isPublished = true;
 }
 
-console.log(person);
-console.log(clonedPerson);
-console.log(updated);
+// publish(book);
 
-// Shallow copy
-
-
-const addFunction = a => b => a + b;
-console.log(addFunction(1)(2));
-
-const numbers = [1,2,3];
-const index = numbers.indexOf(2);
-const added = [...numbers.slice(0, index), 4, ...numbers.slice(index)];
-
-console.log(numbers);
-console.log(index);
-console.log(added);
-
-
+console.log(book);
