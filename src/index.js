d9d30690a8bf47d2e823c361eb30b2d89fc09d20
Carl Phillips <Carl_Phillips@epam.com>
2020-06-16 14:44:41 +0200

    Using Object.assign + examples.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-import { pipe } from "lodash/fp";
-// import { add } from "./currying";
+const person = { name: "John",
+    address: {
+        country: "USA",
+        city: "San Francisco"
+    }
+};
+const clonedPerson = Object.assign({}, person);
+const updated = {
+    ...person,
+    address: {
+        ...person.address,
+        city: "`New York`"
+    },
+    name: "Carl",
+    age: 30
+}
 
-let input = "    Javascript   ";
-let output = "<div>" + input.trim() + "</div>";
+console.log(person);
+console.log(clonedPerson);
+console.log(updated);
 
-const trim = str => str.trim();
-const wrap = type => str => `<${type}>${str}</${type}>`;
-const toLowerCase = str => str.toLowerCase();
-const transform = pipe(trim, toLowerCase, wrap("span"));
+// Shallow copy
 
-// console.log(wrapInDiv(toLowerCase(trim(input))));
-console.log(transform(input));
+
+const addFunction = a => b => a + b;
+console.log(addFunction(1)(2));
+
+const numbers = [1,2,3];
+const index = numbers.indexOf(2);
+const added = [...numbers.slice(0, index), 4, ...numbers.slice(index)];
+
+console.log(numbers);
+console.log(index);
+console.log(added);
+
+
